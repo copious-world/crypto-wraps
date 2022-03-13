e763a87950db9321915a1fd363fc7a7f43bd0c05
Richard Albert Leddy <richardaleddy@gmail.com>
2022-03-13 12:32:30 -0700

    Make use of roll-right
    Add dependency  expose browser_code

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const crypto_wraps = require('./lib/crypto-wraps')
 
 module.exports = crypto_wraps
+
+
+
+
+
+
+const {browser_code} = require('roll-right')
+module.exports.browser_code = () => { return browser_code(__dirname) }
