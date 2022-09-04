3b8fe05f046ec8ac0dc5c5be8eb67ce2e5af6f95
Richard Albert Leddy <richardaleddy@gmail.com>
2022-09-04 16:58:18 -0700

    remove dependency on roll-right

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 const crypto_wraps = require('./lib/crypto-wraps')
 
 module.exports = crypto_wraps
-
-
-
-
-
-
-const {browser_code} = require('roll-right')
-module.exports.browser_code = () => { return browser_code(__dirname) }
